50ed56fd0036be59e1ed702338836a5c06255675
Moshe Karako <moshe@karako.co.il>
2020-03-31 22:41:24 +0300

    Added font awesome and react strap

--- src/components/ToolbarButton/index.js
+++ src/components/ToolbarButton/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './ToolbarButton.css';
 
 export default function ToolbarButton(props) {
     const { icon } = props;
     return (
-      <i className={`toolbar-button ${icon}`} />
+        <i className={`toolbar-button`}><FontAwesomeIcon icon={['fas', icon]} /></i>
     );
 }
